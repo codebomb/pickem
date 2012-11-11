7d19f8e0e1f78655b885940e02b4a1704be42dd8
cb <gitbomb@gmail.com>
2012-11-11 12:11:21 -0600

    generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pickem::Application.config.secret_token = '3162555314461e476f0d98546ce28ffa1235247aaadd107a73f5dcf7a2915117be4b4b2246e5d080213c017528c52523bc216c737adad29da33403a41b92d62a'
